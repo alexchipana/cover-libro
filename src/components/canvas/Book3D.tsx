a6f8e7d93fb00f01ed047183a823218d4f45acfc
Alex Chipana <alexchipana@gmail.com>
2026-01-30 19:11:30 -0400

    Simplify book geometry: clear spine-cover structure

--- src/components/canvas/Book3D.tsx
+++ src/components/canvas/Book3D.tsx
@@ -22,90 +22,69 @@ export function Book3D({ config }: Book3DProps) {
   } = config;
 
   const isHardcover = coverType === 'hard';
-  const coverThickness = isHardcover ? 0.08 : 0.03;
-  const pageBlockWidth = width - coverThickness * 2 - spineWidth;
+  const coverThickness = isHardcover ? 0.06 : 0.02;
+  const pageBlockWidth = width - (coverThickness * 2);
 
   useEffect(() => {
     if (groupRef.current) {
-      groupRef.current.rotation.y = -Math.PI / 4;
+      groupRef.current.rotation.y = -Math.PI / 5;
     }
   }, []);
 
-  const spineMaterial = new THREE.MeshStandardMaterial({
-    color: spineTexture ? 0xffffff : 0x333333,
-    roughness: 0.5,
-    metalness: 0.1,
-  });
-
-  const coverMaterial = new THREE.MeshStandardMaterial({
-    color: coverTexture ? 0xffffff : 0x222222,
-    roughness: 0.6,
-    metalness: 0.1,
-  });
-
-  const pageMaterial = new THREE.MeshStandardMaterial({
-    color: new THREE.Color(pageEdgeColor),
-    roughness: 0.8,
-    metalness: 0,
-  });
+  const materials = {
+    spine: new THREE.MeshStandardMaterial({ color: 0x2a2a2a, roughness: 0.5, metalness: 0.1 }),
+    cover: new THREE.MeshStandardMaterial({ color: 0x1a1a1a, roughness: 0.6, metalness: 0.1 }),
+    pages: new THREE.MeshStandardMaterial({ color: new THREE.Color(pageEdgeColor), roughness: 0.9 }),
+  };
 
   useEffect(() => {
-    if (coverTexture) {
-      const loader = new THREE.TextureLoader();
-      loader.load(coverTexture, (texture) => {
-        texture.colorSpace = THREE.SRGBColorSpace;
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        coverMaterial.map = texture;
-        coverMaterial.color = new THREE.Color(0xffffff);
-        coverMaterial.needsUpdate = true;
+    if (coverTexture && typeof coverTexture === 'string') {
+      new THREE.TextureLoader().load(coverTexture, (tex) => {
+        tex.colorSpace = THREE.SRGBColorSpace;
+        tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+        materials.cover.map = tex;
+        materials.cover.color = new THREE.Color(0xffffff);
+        materials.cover.needsUpdate = true;
       });
     } else {
-      coverMaterial.map = null;
-      coverMaterial.color = new THREE.Color(0x222222);
-      coverMaterial.needsUpdate = true;
+      materials.cover.map = null;
+      materials.cover.color = new THREE.Color(isHardcover ? 0x1a1a1a : 0x2a2a2a);
+      materials.cover.needsUpdate = true;
     }
-  }, [coverTexture, coverMaterial]);
+  }, [coverTexture, isHardcover]);
 
   useEffect(() => {
-    if (spineTexture) {
-      const loader = new THREE.TextureLoader();
-      loader.load(spineTexture, (texture) => {
-        texture.colorSpace = THREE.SRGBColorSpace;
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        spineMaterial.map = texture;
-        spineMaterial.color = new THREE.Color(0xffffff);
-        spineMaterial.needsUpdate = true;
+    if (spineTexture && typeof spineTexture === 'string') {
+      new THREE.TextureLoader().load(spineTexture, (tex) => {
+        tex.colorSpace = THREE.SRGBColorSpace;
+        tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
+        materials.spine.map = tex;
+        materials.spine.color = new THREE.Color(0xffffff);
+        materials.spine.needsUpdate = true;
       });
     } else {
-      spineMaterial.map = null;
-      spineMaterial.color = new THREE.Color(0x333333);
-      spineMaterial.needsUpdate = true;
+      materials.spine.map = null;
+      materials.spine.color = new THREE.Color(0x2a2a2a);
+      materials.spine.needsUpdate = true;
     }
-  }, [spineTexture, spineMaterial]);
+  }, [spineTexture]);
 
   return (
     <group ref={groupRef} position={[0, 0, 0]}>
       <group position={[0, height / 2 - 0.5, 0]}>
-        <mesh position={[-width / 2 + coverThickness + spineWidth / 2, 0, 0]}>
-          <boxGeometry args={[spineWidth, height, coverThickness]} />
-          <primitive object={spineMaterial} attach="material" />
+        <mesh position={[-pageBlockWidth / 2 - coverThickness, 0, 0]}>
+          <boxGeometry args={[coverThickness, height, spineWidth]} />
+          <primitive object={materials.cover} attach="material" />
         </mesh>
 
-        <mesh position={[width / 2 - coverThickness / 2, 0, 0]}>
-          <boxGeometry args={[coverThickness, height, spineWidth + 0.01]} />
-          <primitive object={coverMaterial} attach="material" />
-        </mesh>
-
-        <mesh position={[-width / 2 + coverThickness / 2, 0, 0]}>
-          <boxGeometry args={[coverThickness, height, spineWidth + 0.01]} />
-          <primitive object={coverMaterial} attach="material" />
+        <mesh position={[pageBlockWidth / 2 + coverThickness, 0, 0]}>
+          <boxGeometry args={[coverThickness, height, spineWidth]} />
+          <primitive object={materials.cover} attach="material" />
         </mesh>
 
         <mesh position={[0, 0, 0]}>
-          <boxGeometry args={[pageBlockWidth - 0.02, height - 0.02, spineWidth - 0.02]} />
-          <primitive object={pageMaterial} attach="material" />
+          <boxGeometry args={[pageBlockWidth, height - 0.02, spineWidth - 0.01]} />
+          <primitive object={materials.pages} attach="material" />
         </mesh>
       </group>
     </group>
